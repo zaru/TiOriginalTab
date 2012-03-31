d04a7e276aeab5e3dc85c3e36f5b415bb6ab463a
sakuraba <zarutofu@gmail.com>
2012-04-01 04:36:49 +0900

    とりあえずWindowの設定

--- Resources/app.js
+++ Resources/app.js
@@ -1,6 +1,12 @@
 var tabGroup = Ti.UI.createTabGroup();
 
-var win = Ti.UI.createWindow();
+var win = Ti.UI.createWindow({
+	backgroundColor:'#fff'
+});
+var label = Ti.UI.createLabel({
+	text:'Win1',
+});
+win.add(label);
 win.hideTabBar();
 
 var tab = Ti.UI.createTab({
