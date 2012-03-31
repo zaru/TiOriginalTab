441413e329c642c576e01620d9e1a887ce2c2fa7
sakuraba <zarutofu@gmail.com>
2012-04-01 04:38:14 +0900

    もう一つTabを追加

--- Resources/app.js
+++ Resources/app.js
@@ -15,4 +15,19 @@ var tab1 = Ti.UI.createTab({
 });
 tabGroup.addTab(tab1);
 
+var win2 = Ti.UI.createWindow({
+	backgroundColor:'#fff'
+});
+var label2 = Ti.UI.createLabel({
+	text:'Win2',
+});
+win2.add(label2);
+win2.hideTabBar();
+
+var tab2 = Ti.UI.createTab({
+	title:'test',
+	window:win2
+});
+tabGroup.addTab(tab2);
+
 tabGroup.open();
